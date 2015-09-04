1f71a3245cb17587c8de8062086c4b805450896f
unknown <albrown1@697-334-Aldburg.sf.k12.internal>
2015-09-04 10:47:21 -0700

    Add light

--- Lightning.pde
+++ Lightning.pde
@@ -1,20 +1,24 @@
 int startX = 150;
 int startY = 0;
 int endX = 150;
-int endY = 150;
+int endY = 0;
+
+
 void setup()
 {
   size(300,300);
   strokeWeight(1);
   background(127);
+  draw();
 }
+
 void draw()
 {
 	//stroke((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
 	stroke(255,0,255);
 	while (endY<300)
 	{
-		endX = startX +(int)(Math.random()*10)-9;
+		endX = startX +(int)(Math.random()*18-9);
 		endY = startY +(int)(Math.random()*9);
 		line(startX,startY,endX,endY);	
 		startX=endX;
@@ -25,7 +29,7 @@ void draw()
 void mousePressed()
 {
 
-	startX = 150;
+	startX = 100;
 	startY = 0;
 	endX = 150;
 	endY = 150;
--- build-tmp/source/Lightning.java
+++ build-tmp/source/Lightning.java
@@ -17,20 +17,24 @@ public class Lightning extends PApplet {
 int startX = 150;
 int startY = 0;
 int endX = 150;
-int endY = 150;
+int endY = 0;
+
+
 public void setup()
 {
   size(300,300);
   strokeWeight(1);
   background(127);
+  draw();
 }
+
 public void draw()
 {
 	//stroke((int)(Math.random()*255), (int)(Math.random()*255), (int)(Math.random()*255));
 	stroke(255,0,255);
 	while (endY<300)
 	{
-		endX = startX +(int)(Math.random()*10)-9;
+		endX = startX +(int)(Math.random()*18-9);
 		endY = startY +(int)(Math.random()*9);
 		line(startX,startY,endX,endY);	
 		startX=endX;
@@ -41,7 +45,7 @@ public void draw()
 public void mousePressed()
 {
 
-	startX = 150;
+	startX = 100;
 	startY = 0;
 	endX = 150;
 	endY = 150;
